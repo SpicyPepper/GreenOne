5e12cc3eaac3407561b8dcf2a8470386123c71cd
Joel H <joelhun@uw.edu>
2015-01-29 06:25:21 -0800

    Fully new game
    Here's a sample game i just made.

--- app.ts
+++ app.ts
@@ -1,34 +1,55 @@
-﻿module Game {
-    export class GreenOne {
-        game: Phaser.Game;
-        constructor() {
-            this.game = new Phaser.Game(720, 720, Phaser.AUTO, 'content', {
-                create: this.create, preload: this.preload
-            });
+﻿
+var game = new Phaser.Game(800, 512, Phaser.AUTO, 'greenone', true, false, false, Phaser.Physics.ARCADE);
+var hero;
+var cursors;
+var background;
+
+var mainState = {
+    preload: function () {
+        game.load.image('background', 'visuals/bkgrnd_sand.png');
+        game.load.spritesheet('hero', '/visuals/test_run.png', 128, 128);
+
+
+    },
+    
+    create: function () {
+        
+        game.world.setBounds(0, 0, 800, 512);
+        background = game.add.tileSprite(0, 0, 1024, 512, 'background');
+        hero = game.add.sprite(300, 200, 'hero');
+        hero.animations.add('run');
+        hero.animations.play('run', 40, true);
+        
+        game.physics.enable(hero, Phaser.Physics.ARCADE);
+        hero.body.collideWorldBounds = true;
+        game.camera.follow(hero);
+        cursors = game.input.keyboard.createCursorKeys();    
+        //Phaser does all scaling because of this line.
+    },
+
+    update: function () {
+        background.tilePosition.x -= 2;
+
+        hero.body.velocity.x = 0;
+        hero.body.velocity.y = 0;
+
+        if (cursors.left.isDown) {
+            hero.body.velocity.x = -240;
+        } else if (cursors.right.isDown) {
+            hero.body.velocity.x = 240;
         }
-
-        preload() {
-            //Graphics
-            this.game.load.image("space_background", "/Visuals/Space_background.jpeg");
-            this.game.load.image("space_background_planet", "/Visuals/Space_background_planet.png");
-            this.game.load.image("title", "/Visuals/Title.png");
-            this.game.load.image("pepper", "/Visuals/Spicy_Pepper.gif"); 
-
-            //spritesheets
-          //  this.game.load.atlasXML("Scroller_enemy_walk", "Visuals/Scroller_enemy_walk.png"); //used for animating sprite sheets
-           // this.game.load.atlasXML("Scroller_enemy_idle", "Visuals/Scroller_enemy_idle.png");
-
-            // audio
-            this.game.load.audio("Title_DnB", "Audio/Title_DnB.mp3");
+        if (cursors.up.isDown) {
+            hero.body.velocity.y = -240;
+        } else if (cursors.down.isDown) {
+            hero.body.velocity.y = 240;
         }
+    },
 
-        create() {
-            this.game.state.add("TitleScreenState", TitleState.TitleScreenState, true); //true runs this state right away. it's running when it's creating
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //Phaser does all scaling because of this line.
-        }
-    }
-}
+    render: function () {
+        game.debug.cameraInfo(game.camera, 500, 32);
+        game.debug.spriteCoords(hero, 32, 32);
+    },
+};
 
-window.onload = () => {
-    var game = new Game.GreenOne();
-}
+game.state.add('main', mainState);
+game.state.start('main');

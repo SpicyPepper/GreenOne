1b792301545b9de69be6806bfc7b1731a81fe020
KCSesh <KyleC.Sessions1@gmail.com>
2015-02-02 14:40:28 -0800

    Updated gravity :D
    Changes gravity when spacebar is pushed. Only changes when hero is on
    the floor or the ceiling.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var hero;
 var cursors;
 var background;
 var layer;
+var gravityButton;
 function preload() {
     game.load.tilemap('level1', 'resources/level1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles-1', 'resources/tiles-1.png');
@@ -11,33 +12,41 @@ function preload() {
     game.load.spritesheet('hero', '/visuals/test_run.png', 128, 128);
 }
 function create() {
-    game.world.setBounds(0, 0, 800, 512);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.world.setBounds(0, 0, 2000, 512);
     //adds tilesprite (tilespritet necessary for parallax scrolling);
     background = game.add.tileSprite(0, 0, 1024, 512, 'background');
     map = game.add.tilemap('level1');
     //set collision
     map.addTilesetImage('tiles-1');
-    // map.setCollisionByExclusion([]);
+    map.setCollisionByExclusion([]);
     layer = map.createLayer('Tile Layer 1');
-    // layer.debug = true;
+    layer.debug = true;
     layer.resizeWorld();
-    game.physics.arcade.gravity.y = 250;
+    game.physics.arcade.gravity.y = 10000;
     //Phaser.Physics.Arcade.collideSpriteVsTilemapLayer(hero, 
     //hero sprite
     hero = game.add.sprite(300, 200, 'hero');
     hero.animations.add('run');
     hero.animations.play('run', 70, true);
     game.physics.enable(hero, Phaser.Physics.ARCADE);
+    hero.body.bounce.y = 0.2;
     hero.body.collideWorldBounds = true;
     game.camera.follow(hero);
     cursors = game.input.keyboard.createCursorKeys();
+    gravityButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     //Phaser does all scaling because of this line.
 }
 //mapeditor.org for tiles
 function update() {
+    game.physics.arcade.collide(hero, layer);
     background.tilePosition.x -= 2;
-    hero.body.velocity.x = 0;
+    hero.body.velocity.x = 240;
     hero.body.velocity.y = 0;
+    //if (gravityButton.isDown) {
+    if (gravityButton.isDown && hero.body.blocked.down || gravityButton.isDown && hero.body.blocked.up) {
+        game.physics.arcade.gravity.y = game.physics.arcade.gravity.y * -1;
+    }
     if (cursors.left.isDown) {
         hero.body.velocity.x = -240;
     }
--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ var hero;
 var cursors;
 var background;
 var layer;
+var gravityButton;
 
 
     function preload() {
@@ -19,7 +20,8 @@ var layer;
     
     function create() {
         
-        game.world.setBounds(0, 0, 800, 512);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.setBounds(0, 0, 2000, 512);
 
         //adds tilesprite (tilespritet necessary for parallax scrolling);
         background = game.add.tileSprite(0, 0, 1024, 512, 'background');
@@ -28,14 +30,14 @@ var layer;
         //set collision
         map.addTilesetImage('tiles-1');
 
-       // map.setCollisionByExclusion([]);
+        map.setCollisionByExclusion([]);
 
         layer = map.createLayer('Tile Layer 1');
-       // layer.debug = true;
+        layer.debug = true;
 
         layer.resizeWorld();
 
-        game.physics.arcade.gravity.y = 250;
+        game.physics.arcade.gravity.y = 10000;
 
 
         //Phaser.Physics.Arcade.collideSpriteVsTilemapLayer(hero, 
@@ -45,19 +47,27 @@ var layer;
         hero.animations.play('run', 70, true);
         game.physics.enable(hero, Phaser.Physics.ARCADE);
        
+        hero.body.bounce.y = 0.2;
+       
         hero.body.collideWorldBounds = true;
         game.camera.follow(hero);
 
-        cursors = game.input.keyboard.createCursorKeys();    
+        cursors = game.input.keyboard.createCursorKeys();  
+        gravityButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);  
        //Phaser does all scaling because of this line.
     }
     //mapeditor.org for tiles
     function update() {
+        game.physics.arcade.collide(hero, layer);
         background.tilePosition.x -= 2;
 
-        hero.body.velocity.x = 0;
+        hero.body.velocity.x = 240;
         hero.body.velocity.y = 0;
 
+        //if (gravityButton.isDown) {
+        if (gravityButton.isDown && hero.body.blocked.down || gravityButton.isDown && hero.body.blocked.up){
+            game.physics.arcade.gravity.y = game.physics.arcade.gravity.y * -1;
+        }
         if (cursors.left.isDown) {
             hero.body.velocity.x = -240;
         } else if (cursors.right.isDown) {
